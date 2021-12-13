ddbf639c5954ae7fc29190522be9e478173cd598
Scott Xu <525125scott@gmail.com>
2021-12-13 09:25:21 -0500

    Re-order imports

--- unit_tests/tests/parser/parser.cpp
+++ unit_tests/tests/parser/parser.cpp
@@ -1,6 +1,6 @@
-#include "gtest/gtest.h"
 #include "qasmtools/parser/parser.hpp"
 #include "qasmtools/ast/semantic.hpp"
+#include "gtest/gtest.h"
 
 #include <sstream>
 
--- unit_tests/tests/utils/angle.cpp
+++ unit_tests/tests/utils/angle.cpp
@@ -1,5 +1,5 @@
+#include "qasmtools/utils/angle.hpp"
 #include "gtest/gtest.h"
-#include <qasmtools/utils/angle.hpp>
 
 using namespace qasmtools::utils;
 
